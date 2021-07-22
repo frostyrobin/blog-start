61dca02b5c3d7b48df1c8347236c856cfc685a89
Frostyrobin <yapjanjeric@gmail.com>
2021-07-22 19:54:26 +0800

    Changed blog title and headers from Angela's Blog to BlogStart

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import sqlalchemy
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -29,7 +29,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
